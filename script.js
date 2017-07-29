bb29095b9229b6955b284d0b206b68fe47aae1d2
John <john.torres.hurtado@gmail.com>
2017-07-29 14:12:07 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// event listener on load trigger counter
+$(document).on("pageload",clock);
+// timer
+let time = 0;
+var clock = setInterval(function () {
+   $('.time').text (time + ' secs');
+	time ++;
+},1000);
+
+// score tracker
+let points = 0.5;
+$('.card').click(function(){
+	
+	if (points < 9.5){
+	$('.stars').text(Math.round(points)+ ' moves ' +  '  ★★★');
+	}
+	
+	if (points >= 9.5){
+	$('.stars').text(Math.round(points)+ ' moves ' +  '  ★★☆');
+	}
+	
+	if (points >= 13.5){
+	$('.stars').text(Math.round(points)+ ' moves ' + '  ★☆☆');
+	}
+	points += 0.5;
+});
+
+//reset button
+$('.reset').click(function() {
+    window.location.reload();
+});
