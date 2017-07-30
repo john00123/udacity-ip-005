aa1a2cf6732a092cfc643d28aec1328c0876ab72
John <john.torres.hurtado@gmail.com>
2017-07-29 19:05:57 -0700

    changes

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 // event listener on load trigger counter
 $(document).on("pageload",clock);
+
+
 // timer
 let time = 0;
 var clock = setInterval(function () {
-   $('.time').text (time + ' secs');
+  $('.time').text (time + ' secs');
 	time ++;
 },1000);
 
-// score tracker
+
+// score tracker code.
 let points = 0.5;
 $('.card button').click(function(){
 
 	if (points < 9.5){
-	$('.stars').text(Math.round(points) + ' moves' + ' ★★★' );
+	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9733;</span>' );
 	}
 
 	if (points >= 9.5){
-	$('.stars').text(Math.round(points)+ ' moves ' + ' ★★☆' );
-	}
+	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9734;</span>' );
+  }
 
 	if (points >= 13.5){
-	$('.stars').text(Math.round(points)+ ' moves ' + ' ★☆☆');
-	}
+	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9734; &#9734;</span>' );
+  }
+
 	points += 0.5;
+
 });
+
 
 //reset button
 $('.reset').click(function() {
