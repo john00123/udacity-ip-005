28d36f7f15972ad3b941022fc170d29f9e903857
John <john.torres.hurtado@gmail.com>
2017-07-29 19:36:22 -0700

    added function jquery

--- script.js
+++ script.js
@@ -12,9 +12,25 @@ var clock = setInterval(function () {
 
 // score tracker code.
 let points = 0.5;
+var array = [];
+var index = 0;
 $('.card button').click(function(){
+  var select = $( this ).text();
+  array[index] += text(select);
 
-	if (points < 9.5){
+  if (index == 1){
+    if(array[0] == array[1]){
+      $(body).css('background-color','blue');
+      index = 0;
+    } else{
+      $(body).css('background-color','red');
+    }
+  };
+  index += 1;
+  $(h1).text(index);
+
+  // score tracker code.
+  if (points < 9.5){
 	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9733;</span>' );
 	}
 
