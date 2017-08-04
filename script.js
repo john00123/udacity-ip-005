4710bac8f3a334d858f0a290416ecad62fccc42a
John <john.torres.hurtado@gmail.com>
2017-08-04 00:32:47 -0700

    working logic and random assigment

--- script.js
+++ script.js
@@ -1,79 +1,98 @@
-// event listener on load trigger counter
-$(document).on("pageload",clock);
 
+// event listener on load trigger counter
+$(document).on('pageload', clock);
 
 // timer
-let time = 0;
+var time = 0;
 var clock = setInterval(function () {
-	$('.time').text (time + ' secs');
-	time ++;
-},1000);
+  $('.time').text(time + ' secs');
+  time++;
+}, 1000);
+
+// card generator and randomizer
+var cards = ['Boat', 'Boat', 'Car', 'Car', 'House', 'House', 'Light', 'Light', 'Lamp', 'Lamp', 'Rug', 'Rug', 'Plant', 'Plant', 'Book', 'Book'];
+
+var max = cards.length;
+var min = 1;
+
+	while (max >= min){
+		var randomizer = Math.floor( Math.random()*(max-min+1));
+		var loopValue = cards[randomizer];
+		$('.table').append('<div class =\'card\'>' + loopValue + '</div>'),
+		max --,
+		cards.splice(randomizer,1);
+	}
 
 // score tracker code.
-let points = 0.5; //first click needs to increment 1 and 2 clicks will equal one turn after that.
+var points = 0.5; //first click needs to increment 1 and 2 clicks will equal one turn after that.
 var correctAnswers = 0;
 var array = [];
 var index = 0;
 
-$('.card').click(function(){
-	var select = $(this).text();
-	// takes values from cards, and adds them to an array for comparison.
-	array.push(select);
-	$('h1').text(array);
-	$(this).addClass('selected');
-	index += 1;
+$('.card').click(function () {
+  var select = $(this).text();
 
-	// registers clicks and takes value, then it compares it to each other to check if it's correct or not.
-	if (index == 2){
-		$('.card').addClass('stop');
-		if(array[0] == array[1]){
-			setTimeout(function(){
-			$('.selected').addClass('good');
-			$('.card').removeClass('stop');
-			},400);
-			index = 0;
-			array = [];
-			// adds to number of correct answers
-			correctAnswers +=1;
-		}
-		if(array[0] != array[1]){
-			setTimeout(function(){
-				$('.card').removeClass('stop');
-				$('*').removeClass('selected');
-			},800);
-			index = 0;
-			array = [];
-		}
-	};
+  // takes values from cards, and adds them to an array for comparison.
+  array.push(select);
+  $('h1').text(array);
+  $(this).addClass('selected');
+  index += 1;
 
+  // registers clicks and takes value, then it compares it to each other to check if it's correct or not.
+  if (index == 2) {
+    $('.card').addClass('stop');
+    if (array[0] == array[1]) {
+      setTimeout(function () {
+        $('.selected').addClass('good');
+        $('.card').removeClass('stop');
+      }, 400);
 
+      index = 0;
+      array = [];
 
+      // adds to number of correct answers
+      correctAnswers += 1;
+    }
 
-	// registers the number of correct pais in the board. Once all of them have been completed it triggers alert and stops clock.
-	if (correctAnswers === ($('.card').length/2)){
-		setTimeout(function() { alert('You win. It took you ' +(time -1) + ' seconds and '+ Math.round(points-1) +' moves. Congratulations!'); }, 1800); //added delay to allow animation to finish
-		clearInterval(clock);
-	};
+    if (array[0] != array[1]) {
+      setTimeout(function () {
+        $('.card').removeClass('stop');
+        $('*').removeClass('selected');
+      }, 800);
 
-	// score tracker code.
-	if (points < 9.5){
-	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9733;</span>' );
-	}
+      index = 0;
+      array = [];
+    }
+  }
 
-	if (points >= 9.5){
-	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9734;</span>' );
-	}
+  // registers the number of correct pais in the board. Once all of them have been completed it triggers alert and stops clock.
+  if (correctAnswers === ($('.card').length / 2)) {
+    setTimeout(function () {
+      alert('You win. It took you ' + (time - 1) + ' seconds and ' + Math.round(points - 1) + ' moves. Congratulations!');
+    }, 1800);
 
-	if (points >= 13.5){
-	$('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9734; &#9734;</span>' );
-	}
+    //added delay to allow animation to finish\
+    clearInterval(clock);
+  }
 
-	points += 0.5;
+  // score tracker code.
+  if (points < 9.5) {
+    $('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9733;</span>');
+  }
+
+  if (points >= 9.5) {
+    $('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9733; &#9734;</span>');
+  }
+
+  if (points >= 13.5) {
+    $('.stars').html(Math.round(points) + ' moves <span>&#9733; &#9734; &#9734;</span>');
+  }
+
+  points += 0.5;
 
 });
 
-
 //reset button
-$('.reset').click(function() {
-		window.location.reload();
+$('.reset').click(function () {
+  window.location.reload();
 });
